4e75cd5ac070f61bbbab2727cdd656189d5803d1
Tran Dat <dat27021003@gmail.com>
2026-03-01 17:30:08 +0700

    fix: remove hardcoded localhost from public/config.js - this was overriding production URL

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
-window.env = {
-    VITE_API_ROOT: "http://localhost:8017",
-    VITE_GOOGLE_CLIENT_ID: "339787734370-al58slqdejnv5r8j41p9r54dsmc3vq9f.apps.googleusercontent.com"
-};
+// Runtime config cho Docker - CHỈ ghi đè khi Docker inject giá trị thực
+// Khi deploy lên Vercel, file này KHÔNG nên set giá trị
+// vì nó sẽ override URL production trong constants.js
+window.env = {};
