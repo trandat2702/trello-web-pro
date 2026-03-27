ae74cbe3d842aff271f762530f7abc41412e6aea
Tran Dat <dat27021003@gmail.com>
2026-03-28 02:44:32 +0700

    fix_socket_vercel

--- src/socketClient.js
+++ src/socketClient.js
@@ -2,4 +2,6 @@
 // https://socket.io/how-to/use-with-react
 import { io } from 'socket.io-client'
 import { API_ROOT } from '~/utils/constants'
-export const socketIoInstance = io(API_ROOT)
+// Trên Vercel production, API_ROOT=' /api ' sẽ làm Socket.io nhầm tưởng là namespace
+// Nên ta phải truyền nguyên URL hiện tại (window.location.origin) để nó gọi đúng vào /socket.io
+export const socketIoInstance = io(import.meta.env.PROD ? window.location.origin : API_ROOT)
